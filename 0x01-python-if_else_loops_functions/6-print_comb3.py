ec763f52bb0173ecb6a67068e82d7eea83dba893
owuama1 <kelechiowuama24@gmail.com>
2023-12-14 16:30:59 -0800

    a little editiing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(9):
-    for j in range(i + 1, 10):
-        print("{:02d}".format(i * 10 + j), end=(", " if i < 8 or j < 9 else "\n"))
+for i in range(10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        print(f"{i}{j}", end=", " if i < 8 or (i == 8 and j < 9) else "\n")
