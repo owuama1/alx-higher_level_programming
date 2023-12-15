c62ae58dee176ff8f99df01b78c556b768c235f6
owuama1 <kelechiowuama24@gmail.com>
2023-12-14 16:33:57 -0800

    back to the drawing boar

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(10):
     for j in range(1, 10):
         if i >= j:
             continue
-        print(f"{i}{j}", end=", " if i < 8 or (i == 8 and j < 9) else "\n")
+        print("{}{}".format(i, j), end=", " if i < 8 or (i == 8 and j < 9) else "\n")
