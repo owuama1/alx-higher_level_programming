74ac8c7a40e93fb53117ff2557eeb44ccd03458e
owuama1 <kelechiowuama24@gmail.com>
2023-12-20 02:21:27 -0800

    correct 0 argument condition and check pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     num_args = len(argv) - 1
     args_list = argv[1:]
 
-    print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""), end="\n" if num_args != 0 else ".")
-
-    for i, arg in enumerate(args_list, start=1):
-        print("{}: {}".format(i, arg))
+    if num_args != 0:
+        print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""))
+        for i, arg in enumerate(args_list, start=1):
+            print("{}: {}".format(i, arg))
+    else:
+        print("0 arguments.")
