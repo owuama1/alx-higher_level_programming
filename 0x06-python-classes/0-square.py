068ebe16db82d5cac4d3efe4cb7701ed0ffd08f5
owuama1 <kelechiowuama24@gmail.com>
2023-12-20 13:01:21 -0800

    new directory

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square class.
+
+This module defines a simple Square class with no attributes.
+"""
+
+class Square:
+    """Empty class representing a square.
+
+    Attributes:
+        No attributes are defined for this class.
+    """
+    pass
+
