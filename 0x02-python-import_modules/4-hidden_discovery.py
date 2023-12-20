9a77eb3e5df1a3bde4623ae4f91dd996a4dd7351
owuama1 <kelechiowuama24@gmail.com>
2023-12-20 02:47:30 -0800

    check pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import importlib.util
 
+
 def get_module_names(module_path):
     # Load the module
-    module_spec = importlib.util.spec_from_file_location("hidden_4", module_path)
-    module = importlib.util.module_from_spec(module_spec)
-    module_spec.loader.exec_module(module)
+    mod_spec = importlib.util.spec_from_file_location("hidden_4", module_path)
+    module = importlib.util.module_from_spec(mod_spec)
+    mod_spec.loader.exec_module(module)
 
     # Get names from the module and filter out those starting with '__'
     names = [name for name in dir(module) if not name.startswith("__")]
 
     return names
+
 
 if __name__ == "__main__":
     module_path = "hidden_4.pyc"
