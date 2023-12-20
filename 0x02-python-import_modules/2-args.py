f969b75daf69355c56aead2e64144eda2b113be5
owuama1 <kelechiowuama24@gmail.com>
2023-12-20 02:12:04 -0800

    print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    args_list = argv[1:]
+
+    print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""), end="\n" if num_args != 0 else ".")
+
+    for i, arg in enumerate(args_list, start=1):
+        print("{}: {}".format(i, arg))
