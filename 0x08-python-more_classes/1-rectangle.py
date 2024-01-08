22a83d5a2f035004737b60eddf24d6f24b53e705
owuama1 <kelechiowuama24@gmail.com>
2024-01-08 06:11:02 -0800

    change the position of height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """Rectangle class with width and height attributes"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle instance with optional width and height"""
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """Getter method for the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for the height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """Getter method for the width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for the width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
