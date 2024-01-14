b5e12e52fb2098b9944752db8e98ef3dcfde7f2f
owuama1 <kelechiowuama24@gmail.com>
2024-01-14 15:53:51 -0800

    print a text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+The defines a funct for text indentation. The funct takes a string as input
+and prints the text with two new lines after each '.', '?', and ':' character.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints the text with two new lines after each '.', '?', and ':' character.
+
+    :param text: A string containing the input text.
+    :type text: str
+    :raises TypeError: If the input is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuation_chars = ['.', '?', ':']
+
+    current_line = ""
+    for char in text:
+        current_line += char
+        if char in punctuation_chars:
+            print(current_line.strip())
+            print()
+            current_line = ""
+
+    if current_line:
+        print(current_line.strip(), end="")
