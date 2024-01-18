efb0db0d5f549c2911cbd1340417238e2a7a3989
owuama1 <kelechiowuama24@gmail.com>
2024-01-18 01:46:10 -0800

    Use dir obj

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""
+It provides a func to get the list of available attr and methods of an obj
+
+Usage:
+    1. Import the module using: `import module_name`
+    2. Call the function with an obj as an arg to get its attr and methods.
+"""
+
 
 def lookup(obj):
-    attributes_and_methods = []
+    """
+    Returns a list containing attributes and methods of the given object.
 
-    for attribute in dir(obj):
-        if not callable(getattr(obj, attribute)) or attribute.startswith('__'):
-            attributes_and_methods.append(attribute)
+    Parameters:
+        obj (object): The object to inspect.
 
-    return attributes_and_methods
+    Returns:
+        list: A list containing attributes and methods of the object.
+    """
+    return dir(obj)
