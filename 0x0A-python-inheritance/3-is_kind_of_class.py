c11082f2523fd8f3a3cf0c48b6a676d535f20883
owuama1 <kelechiowuama24@gmail.com>
2024-01-18 03:25:26 -0800

    Return T/F if an instance of class or subclass

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module: class_check_module
+
+provides a func to see if an obj is an instance
+of a specified class or its subclasses.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if the object is an instance of, or if the object
+    is an instance of a class that inherited from,
+    the specified class.
+
+    :param obj: The object to check.
+    :param a_class: The class to check against.
+    :return: True if the object is an instance of the specified class
+    or its subclasses; otherwise, False.
+    """
+    return isinstance(obj, a_class)
