372657f4219b2f204a7bbcfd7bf8bb6ec16ae9b7
owuama1 <kelechiowuama24@gmail.com>
2024-01-25 12:36:15 -0800

    add def to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,10 +4,12 @@ Module Name: base
 Description: Contains the definition of the Base class.
 """
 
+import json
+
 
 class Base:
     """
-    Base class for managing id attribute in other classes.
+    Base class for other classes.
     """
 
     __nb_objects = 0
@@ -17,10 +19,25 @@ class Base:
         Constructor for the Base class.
 
         Args:
-            id (int): Unique identifier for the instance.
+            id (int): Unique identifier for the base object.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Convert a list of dictionaries to a JSON string.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+
+        Returns:
+            str: JSON string representation of the list of dictionaries.
+        """
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
