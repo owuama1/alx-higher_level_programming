19796e7897d51b3cb710281409d6bbcbc3e76dfd
owuama1 <kelechiowuama24@gmail.com>
2024-01-25 05:21:45 -0800

    override th str method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,14 @@ class Rectangle(Base):
         """
         for _ in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle instance.
+
+        Returns:
+            str: Formatted string with information about the rectangle.
+        """
+        return ("[Rectangle] ({}) {}/{} - {}"
+                "/{}".format(self.id, self.__x, self.__y,
+                             self.__width, self.__height))
