987966c43288f8c51912e08e6b61fe9fbc34d779
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:28:45 +0000

    compute the score average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,8 @@
+/*
+Script: Compute Score Average
+Description: This script computes the score average of all records in the table second_table.
+*/
+
+-- SQL: Compute the average score
+SELECT AVG(score) AS average
+FROM `second_table`;
