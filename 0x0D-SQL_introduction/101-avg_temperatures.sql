c9b3cb75ee830c8833622f81672b9b6151e9eb87
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:39:30 +0000

    display the average temp

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Display the average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(temperature) AS avg_temperature_fahrenheit
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temperature_fahrenheit DESC;
