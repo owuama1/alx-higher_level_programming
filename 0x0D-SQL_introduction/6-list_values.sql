9adbf80b656f24f05e70c73d2ccd609a3e3a75d3
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 10:45:05 +0000

    list all rows

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,8 @@
+/*
+Script: List All Rows of a Table
+Description: This script lists all rows of the table first_table from the database hbtn_0c_0.
+*/
+
+-- SQL: Select all fields from the table first_table
+SELECT *
+FROM hbtn_0c_0.first_table;
