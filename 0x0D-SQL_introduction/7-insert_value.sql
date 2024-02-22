3a0ac706860e4979bc6be500866f4d6bc77d6d2b
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:07:01 +0000

    make little changes

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -4,5 +4,5 @@ Description: This script inserts a new row into the table first_table in the dat
 */
 
 -- SQL: Insert a new row into the table first_table
-INSERT INTO hbtn_0c_0.first_table (id, name)
-VALUES (89, 'Best School');
+INSERT INTO `first_table` (`id`, `name`)
+VALUES (89, "Best School");
