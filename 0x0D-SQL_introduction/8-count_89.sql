cfe107aceb0bf331dce37acfb9a2a7e79569a86f
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:09:35 +0000

    put ticks

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -5,5 +5,5 @@ Description: This script displays the number of records with id = 89 in the tabl
 
 -- SQL: Count the number of records with id = 89 in the table first_table
 SELECT COUNT(*)
-FROM hbtn_0c_0.first_table
-WHERE id = 89;
+FROM `first_table`
+WHERE `id` = 89;
