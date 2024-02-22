7adff029d425b13b29aba4be769c412ff85c7622
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:08:30 +0000

    display number of recorda

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,9 @@
+/*
+Script: Count Records with id = 89 in first_table
+Description: This script displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0.
+*/
+
+-- SQL: Count the number of records with id = 89 in the table first_table
+SELECT COUNT(*)
+FROM hbtn_0c_0.first_table
+WHERE id = 89;
