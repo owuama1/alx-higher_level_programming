b1be656a7a9382ae804749449d2e8c4ac72261aa
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:15:31 +0000

    create a second table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,19 @@
+/*
+Script: Create and Populate second_table
+Description: This script creates a table called second_table in the database hbtn_0c_0 and adds multiple rows to it.
+*/
+
+-- SQL: Create table second_table with id INT, name VARCHAR(256), and score INT
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+-- SQL: Insert multiple rows into the table second_table
+INSERT INTO hbtn_0c_0.second_table (id, name, score)
+VALUES 
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
