659caa3f0cc4bf10468106beb2323d1d3f6a02c2
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:03:57 +0000

    insert a new row

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,8 @@
+/*
+Script: Insert New Row into first_table
+Description: This script inserts a new row into the table first_table in the database hbtn_0c_0.
+*/
+
+-- SQL: Insert a new row into the table first_table
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Best School');
