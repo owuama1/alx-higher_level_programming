bff35d7bb80bbb45f45e6028d618441835c695ab
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:25:32 +0000

    update the score of Bob to 10

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,9 @@
+/*
+Script: Update Score of Bob
+Description: This script updates the score of Bob to 10 in the table second_table.
+*/
+
+-- SQL: Update the score of Bob to 10
+UPDATE `second_table`
+SET score = 10
+WHERE name = 'Bob';
