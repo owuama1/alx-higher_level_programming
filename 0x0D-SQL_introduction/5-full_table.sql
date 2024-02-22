0e17406bdaa19a927a064add1bbf0c250da5d103
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 10:43:15 +0000

    print full desc

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+/*
+Script: Print Table Description
+Description: This script prints the full description of the table first_table.
+*/
+
+-- SQL: Prints the full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
