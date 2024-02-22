670e09aacb0fc48d744655bd9818d2d599002c9e
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 10:28:08 +0000

    delete a database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,7 @@
+/*
+Task: Delete MySQL Database
+Description: This script deletes the database 'hbtn_0c_0' if it exists.
+*/
+
+-- SQL: Drop the database if it exists
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
