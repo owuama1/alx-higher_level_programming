72ecf1b36e029a269e24ae26f5f0926afca523fd
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:17:58 +0000

    put ticks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,14 +4,14 @@ Description: This script creates a table called second_table in the database hbt
 */
 
 -- SQL: Create table second_table with id INT, name VARCHAR(256), and score INT
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS `second_table` (
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
 
 -- SQL: Insert multiple rows into the table second_table
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
+INSERT INTO `second_table` (id, name, score)
 VALUES 
 	(1, 'John', 10),
 	(2, 'Alex', 3),
