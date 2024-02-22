592f356a58915efe68897f54550793eab2374b7f
owuama1 <kelechiowuama24@gmail.com>
2024-02-22 11:49:10 +0000

    put ticks

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Display the top 3 cities' temperatures during July and August ordered by temperature (descending)
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
 WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
