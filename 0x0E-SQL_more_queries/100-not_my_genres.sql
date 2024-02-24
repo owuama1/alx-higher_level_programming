0f78216e6916f83b29efc78dc2c2b1668b94523d
owuama1 <kelechiowuama24@gmail.com>
2024-02-24 01:25:09 +0100

    Use the database to list all the genres not linked to the show Dexter

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- List all genres not linked to the show Dexter from the database hbtn_0d_tvshows
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT genre_id
+    FROM tv_show_genres
+    WHERE show_id = (
+        SELECT id
+        FROM tv_shows
+        WHERE title = 'Dexter'
+    )
+)
+ORDER BY name ASC;
