1f1d1c9b3bea036ee94d4f78b964eae8510d4c8f
owuama1 <kelechiowuama24@gmail.com>
2024-06-06 19:32:06 +0000

    print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const size = parseInt(args[0], 10);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
