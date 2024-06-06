15bc64e6ae93e71fb359694c27dc80b09c0eb31e
owuama1 <kelechiowuama24@gmail.com>
2024-06-06 19:23:32 +0000

    change the logic

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,8 +4,4 @@ const args = process.argv.slice(2);
 const firstArg = args[0];
 const secondArg = args[1];
 
-if (firstArg && secondArg) {
-  console.log(`${firstArg} is ${secondArg}`);
-} else {
-  console.log('undefined is undefined');
-}
+console.log(`${firstArg} is ${secondArg}`);
