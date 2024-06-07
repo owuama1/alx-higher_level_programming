152f36cc701dd72d2b314a639f905b9369fa53d6
owuama1 <kelechiowuama24@gmail.com>
2024-06-07 16:21:54 +0000

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-
-
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
 import sys
 import MySQLdb
 
 
 def list_states(username, password, db_name):
-    try:
-        db = MySQLdb.connect(user=username, passwd=password, db=db_name)
-        c = db.cursor()
-        c.execute("SELECT * FROM states")
-        states = c.fetchall()
-        for state in states:
-            print(state)
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-    finally:
-        if 'db' in locals() and db.open:
-            c.close()
-            db.close()
+    """
+    Connects to MySQL database and lists all states in ascending order by id.
+
+    Args:
+        username (str): The MySQL username.
+        password (str): The MySQL password.
+        db_name (str): The database name.
+    """
+    db = MySQLdb.connect(user=username, passwd=password, db=db_name, port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in c.fetchall()]
+    c.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    #if len(sys.argv) != 4:
-     #   print("Usage: ./0-select_states.py <mysql username> <mysql password> <database name>")
-      #  sys.exit(1)
+    # if len(sys.argv) != 4:
+    #    print("Usage: ./0-select_states.py
+    #    <mysql username> <mysql password> <database name>")
+    #    sys.exit(1)
     list_states(sys.argv[1], sys.argv[2], sys.argv[3])
