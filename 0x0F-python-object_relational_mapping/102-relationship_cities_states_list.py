f14def213796c28b2b6f50544a67931b305982bd
owuama1 <kelechiowuama24@gmail.com>
2024-06-07 21:28:18 +0000

    list all City objects from the database

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Lists all City objects from the database hbtn_0e_101_usa.
+Usage: ./101-relationship_cities_list.py
+<mysql username> <mysql password> <database name>
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./101-relationship_cities_list.py "
+              "<mysql username> <mysql password> <database name>")
+        return
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        cities = session.query(City).order_by(City.id).all()
+        for city in cities:
+            print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        session.close()
+
+
+if __name__ == "__main__":
+    main()
