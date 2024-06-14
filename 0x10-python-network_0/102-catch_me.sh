060318e2182f2787c349b3fba2c9f20985d23773
owuama1 <kelechiowuama24@gmail.com>
2024-06-14 08:16:47 +0000

    put L option

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to 0.0.0.0:5000/catch_me and retrieves the response containing "You got me!"
-curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "http://0.0.0.0:5000/catch_me"
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "http://0.0.0.0:5000/catch_me"
