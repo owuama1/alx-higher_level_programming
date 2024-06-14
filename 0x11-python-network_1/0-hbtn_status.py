ff3975975d9d54be190c7f67f3b5b163d7181997
owuama1 <kelechiowuama24@gmail.com>
2024-06-14 08:32:01 +0000

    make some changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,10 @@ Fetches https://alx-intranet.hbtn.io/status and displays the response details.
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    req = urllib.request.Request(url)
-
-    with urllib.request.urlopen(req) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
-        utf8_content = body.decode("utf-8")
         print("Body response:")
-        print("\t- type: {}".format(type(body).__name__))
+        print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(utf8_content))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
