c67d30041a92c7b3c733ba7bcccd099410be2c13
owuama1 <kelechiowuama24@gmail.com>
2024-06-14 14:19:46 +0000

    fetch https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status, displays the body of the response.
+"""
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
