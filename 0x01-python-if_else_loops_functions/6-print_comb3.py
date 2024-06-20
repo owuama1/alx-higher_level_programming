b11b9dac29d80986b96e2b29adf6c0853e1cf5a1
owuama1 <kelechiowuama24@gmail.com>
2024-06-20 15:30:23 +0000

    pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(1, 10):
-        if i >= j:
-            continue
-        print("{}{}".format(i, j), end=", " if i < 8 or (i == 8 and j < 9) else "\n")
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
