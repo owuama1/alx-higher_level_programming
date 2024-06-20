d5e191e35ae7d768c73ad4646c5fe362da3b97fd
owuama1 <kelechiowuama24@gmail.com>
2024-06-20 16:52:31 +0000

    edit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
-    return;
-  }
-
-  const films = JSON.parse(body).results;
-  const wedgeAntillesMovies = films.filter(film =>
-    film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-  );
-
-  console.log(wedgeAntillesMovies.length);
 });
