e766ad69ce1b6b1c2ba34da98cc029604e6dfcc5
owuama1 <kelechiowuama24@gmail.com>
2024-06-20 17:04:21 +0000

    some changes

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,13 @@
--- List all genres in hbtn_0d_tvshows_rate by their rating sum
-SELECT tv_genres.name, SUM(tv_show_genres.rating) AS rating_sum
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in ascending order by their rating
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
