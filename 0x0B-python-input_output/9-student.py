ad3bd33f3cba9e005aec79365d57d5bdb709fa86
owuama1 <kelechiowuama24@gmail.com>
2024-06-20 16:19:14 +0000

    changes

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,49 +1,17 @@
 #!/usr/bin/python3
-"""
-Module Name: student
-Description: This module defines a class Student with public instance
-             attributes and a method to retrieve a dictionary representation.
-"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """
-    Class Name: Student
-    Description: Represents a student with first_name, last_name, and age.
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance
-
-        :param first_name: The first name of the student.
-        :param last_name: The last name of the student.
-        :param age: The age of the student.
+        """Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        Retrieves a dictionary representation of a Student instance.
-
-        :return: A dictionary representing the student instance.
-        """
-        def is_serializable(value):
-            """
-            Check if a value is serializable (list, dict, str, int, bool).
-
-            :param value: The value to check.
-            :return: True if the value is serializable, False otherwise.
-            """
-            return (
-                isinstance(value, (list, dict, str, int, bool)) or
-                (isinstance(value, type) and value.__name__ == 'bool')
-            )
-
-        attributes = vars(self)
-        serializable_attributes = {
-            key: value for key, value in attributes.items() if is_serializable(value)
-        }
-        return serializable_attributes
+        """Gets a dictionary representation of the Student"""
+        return self.__dict__
