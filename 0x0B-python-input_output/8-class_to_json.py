efc5d9b3a0e47f20f71ea539bd9f7fb4c5983c82
owuama1 <kelechiowuama24@gmail.com>
2024-06-20 16:18:31 +0000

    edit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,33 +1,7 @@
 #!/usr/bin/python3
-"""
-Module Name: json_serializer
-Description: This module provides a function to create a dictionary
-             description suitable for JSON serialization from an object.
-"""
+"""This module defines a Python class-to-JSON function"""
 
 
 def class_to_json(obj):
-    """
-    Return a dictionary description suitable for JSON serialization.
-
-    :param obj: An instance of a class with serializable attributes.
-    :return: A dictionary representing the serialized object.
-    """
-    attributes = vars(obj)
-    serializable_attributes = {
-        key: value for key, value in attributes.items() if is_serializable(value)
-    }
-    return serializable_attributes
-
-
-def is_serializable(value):
-    """
-    Check if value is serializable (list, dictionary, string, integer, boolean)
-
-    :param value: The value to check.
-    :return: True if the value is serializable, False otherwise.
-    """
-    return (
-        isinstance(value, (list, dict, str, int, bool)) or
-        (isinstance(value, type) and value.__name__ == 'bool')
-    )
+    """Returns the dictionary representation of a simple data structure"""
+    return obj.__dict__
