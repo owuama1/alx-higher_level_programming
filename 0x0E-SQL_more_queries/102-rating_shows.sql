e318b7ab3ce95da5e79c945bfc7fdc42be6f5c3f
owuama1 <kelechiowuama24@gmail.com>
2024-06-20 17:03:06 +0000

    edit

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,11 @@
--- List all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(rating) AS rating_sum
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in acending order by the rating
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
